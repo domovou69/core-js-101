ce3073f9707a7a7aeb92fc5923699710a8cf6c23
Mikhail <domovou69@gmail.com>
2021-06-20 01:10:20 +0300

    fix: 38 passed, solved getMovingSum (03-arrays)

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -263,8 +263,14 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const newArray = [];
+  const reducer = function reduce(prev, cur, index) {
+    newArray[index] = prev + cur;
+    return newArray[index];
+  };
+  arr.reduce(reducer, 0);
+  return newArray;
 }
 
 /**
