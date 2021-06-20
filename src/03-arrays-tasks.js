26837a062426ef42896e45ac4380dceb6fc0a594
Mikhail <domovou69@gmail.com>
2021-06-20 23:18:56 +0300

    fix: 50 passed, solved distinct (03-arrays)

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -588,8 +588,11 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+  return arr.filter(onlyUnique);
 }
 
 /**
